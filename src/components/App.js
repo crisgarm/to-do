d329da74ad8eb92779a6da911018c31f61175e09
crisgarm <crisgarm9@gmail.com>
2021-01-26 15:55:30 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../stylesheets/App.css";
+
+class App extends React.Component {
+  // eslint-disable-next-line
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="App">Hola Cris</div>;
+  }
+}
+
+export default App;
